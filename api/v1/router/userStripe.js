5feeba959e7e0cc3e15ea63d1ab340610fa4589d
Angel Castañeda <luis.castaneda@noktos.com>
2025-02-17 08:58:40 -0600

    DONE HANDLER ERROR

--- api/v1/router/userStripe.js
+++ api/v1/router/userStripe.js
@@ -3,17 +3,29 @@ const stripe = require('stripe')(API_STRIPE);
 const router = require("express").Router()
 
 router.post('/create-checkout-session', async (req, res) => {
-  const { payment_data } = req.body
-  const session = await stripe.checkout.sessions.create(payment_data);
-  res.json(session)
+  try {
+
+    const { payment_data } = req.body
+    const session = await stripe.checkout.sessions.create(payment_data);
+    res.json(session)
+
+  } catch (error) {
+    console.log(error)
+    res.json(error)
+  }
 });
 
 router.get("/get-checkout-session", async (req, res) => {
-  const { id_checkout } = req.query
-  const checkout = await stripe.checkout.sessions.retrieve(id_checkout)
-  console.log(checkout)
-  res.json(checkout)
+  try {
 
+    const { id_checkout } = req.query
+    const checkout = await stripe.checkout.sessions.retrieve(id_checkout)
+    console.log(checkout)
+    res.json(checkout)
+
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 module.exports = router
