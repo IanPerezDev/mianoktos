f591a451cdd018f6343ffbea64a036782dd3b9b6
Angel Castañeda <luis.castaneda@noktos.com>
2025-02-17 11:13:38 -0600

    ADD to cors

--- config/auth.js
+++ config/auth.js
@@ -2,8 +2,14 @@ require("dotenv").config()
 
 const API_KEY = process.env.APIKEY
 const API_STRIPE = process.env.API_STRIPE
+const valuesFacturama = {
+  token: process.env.TOKEN_FACTURAMA,
+  useragent: process.env.USERAGENT_FACTURAMA,
+  url: process.env.URL_FACTURAMA
+};
 
 module.exports = {
   API_KEY,
-  API_STRIPE
+  API_STRIPE,
+  valuesFacturama
 }
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const cors = require("cors")
 
 const corsOptions = {
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://viajaconmia.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
 };
