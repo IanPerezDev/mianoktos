5015f68faf20c9089d442b491894521293c7db69
juanincho <sabbaghjuan7@gmail.com>
2025-03-18 21:51:22 -0600

    logs pagos stripe

--- api/v1/router/general/stripe.js
+++ api/v1/router/general/stripe.js
@@ -3,6 +3,7 @@ const { API_STRIPE_TEST } = require("../../../../config/auth");
 const stripe = require('stripe')(API_STRIPE);
 const stripeTest = require('stripe')(API_STRIPE_TEST);
 const router = require("express").Router()
+const { executeTransaction, executeQuery } = require("../../../../config/db");
 
 router.post('/create-checkout-session', async (req, res) => {
   try {
@@ -32,7 +33,7 @@ router.get("/get-checkout-session", async (req, res) => {
 
 router.post("/create-payment-intent-card", async (req, res) => {
   try {
-    const { amount, currency } = req.body;
+    const { amount, currency, id_viajero } = req.body;
     console.log(amount);
     console.log(currency);
     const paymentIntent = await stripeTest.paymentIntents.create({
@@ -40,7 +41,33 @@ router.post("/create-payment-intent-card", async (req, res) => {
       currency,
       payment_method_types: ["card"], // Tarjetas de crédito/débito
     });
+    const query = "INSERT INTO stripe_logs (request, response, precio, id_viajero, created_at, updated_at) VALUES (?, ?, ?, ?, NOW(), NOW());";
+    const params = [JSON.stringify(req.body), JSON.stringify(paymentIntent), amount / 100, id_viajero];
+    const { error } = await executeQuery(query, params);
+    if (error) {
+      throw new Error('Error al almacenar el Log de payment intent');
+    }
+
     res.json({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
+
+router.post("/payment-log-storage", async (req, res) => {
+  try {
+    const { amount, id_viajero, response_payment } = req.body;
+    console.log(amount);
+    console.log(response_payment);
+
+    const query = "INSERT INTO stripe_logs (request, response, precio, id_viajero, created_at, updated_at) VALUES (?, ?, ?, ?, NOW(), NOW());";
+    const params = [JSON.stringify(req.body), JSON.stringify(response_payment), amount / 100, id_viajero];
+    const { error } = await executeQuery(query, params);
+    if (error) {
+      throw new Error('Error al almacenar el Log del payment');
+    }
+
+    res.json({ success: true, message: 'Log almacenado correctamente' });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
