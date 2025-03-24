74d847ade34d4230df1632f338e42a07d04c2ac8
juanincho <sabbaghjuan7@gmail.com>
2025-03-24 01:08:24 -0600

    registro viajeros con multiples empresas

--- api/v1/model/viajeros.js
+++ api/v1/model/viajeros.js
@@ -1,18 +1,48 @@
-const { executeQuery } = require("../../../config/db")
+const { executeQuery, executeTransaction } = require("../../../config/db")
 const { v4: uuidv4 } = require("uuid")
 
 const createViajero = async (viajero) => {
   try {
-    const id_viajero = `via-${uuidv4()}`
-    const query = "INSERT INTO viajeros (id_viajero, id_empresa, primer_nombre, segundo_nombre, apellido_paterno, apellido_materno, correo, fecha_nacimiento, genero, telefono) VALUES (?,?,?,?,?,?,?,?,?,?)"
-    const params = [id_viajero, viajero.id_empresa, viajero.primer_nombre, viajero.segundo_nombre, viajero.apellido_paterno, viajero.apellido_materno, viajero.correo, viajero.fecha_nacimiento, viajero.genero, viajero.telefono]
+    const id_viajero = `via-${uuidv4()}`;
 
-    const response = executeQuery(query, params)
-    return response
+    // Insertar el viajero en la tabla "viajeros"
+    const query = "INSERT INTO viajeros (id_viajero, primer_nombre, segundo_nombre, apellido_paterno, apellido_materno, correo, fecha_nacimiento, genero, telefono) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
+    const params = [
+      id_viajero,
+      viajero.primer_nombre,
+      viajero.segundo_nombre,
+      viajero.apellido_paterno,
+      viajero.apellido_materno,
+      viajero.correo,
+      viajero.fecha_nacimiento,
+      viajero.genero,
+      viajero.telefono,
+    ];
+
+    await executeTransaction(query, params, async (result, connection) => {
+      console.log("Viajero creado correctamente");
+
+      if (viajero.id_empresas && viajero.id_empresas.length > 0) {
+        const query2 = "INSERT INTO viajero_empresa (id_viajero, id_empresa) VALUES (?, ?);";
+
+        for (const id_empresa of viajero.id_empresas) {
+          const params2 = [id_viajero, id_empresa];
+          await connection.execute(query2, params2);
+        }
+        console.log("Relaciones viajero-empresa creadas");
+      }
+    });
+
+    return {
+      success: true,
+      id_viajero: id_viajero,
+    };
   } catch (error) {
     throw error;
   }
-}
+};
+
+
 const readViajero = async () => {
   try {
     const query = "SELECT * FROM viajeros"
