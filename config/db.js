6d95710f5edcafb4e7a6ad68f840018d6eb2a02f
Angel Castañeda <luis.castaneda@noktos.com>
2025-03-24 08:26:22 -0600

    ADD CORS

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
+require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'admin',
-  // password: 'juanin+3',
-  database: 'mia',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 15,
 });
