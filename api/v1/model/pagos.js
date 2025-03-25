0ee175fac9d4300a9a860cbfa81eadb9ec244bfa
Angel Castañeda <luis.castaneda@noktos.com>
2025-03-25 10:56:57 -0600

    ADD split credits

--- api/v1/model/pagos.js
+++ api/v1/model/pagos.js
@@ -67,7 +67,7 @@ const getCreditoAgente = async (body) => {
     console.log(body)
     const { id_agente } = body;
     const query = `
-      SELECT agentes.id_agente, agentes.nombre, agentes.tiene_credito_consolidado, agentes.monto_credito, empresas.id_empresa, empresas.tiene_credito, empresas.monto_credito, empresas.nombre_comercial, empresas.razon_social, empresas.tipo_persona
+      SELECT agentes.id_agente, agentes.nombre, agentes.tiene_credito_consolidado, agentes.monto_credito as monto_credito_agente, empresas.id_empresa, empresas.tiene_credito, empresas.monto_credito as monto_credito_empresa, empresas.nombre_comercial, empresas.razon_social, empresas.tipo_persona
       FROM agentes
       JOIN empresas_agentes ON agentes.id_agente = empresas_agentes.id_agente
       JOIN empresas ON empresas.id_empresa = empresas_agentes.id_empresa
