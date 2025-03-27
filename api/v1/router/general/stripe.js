ad2171eaa2197499585e7f8924167135df9ea3b9
juanincho <sabbaghjuan7@gmail.com>
2025-03-27 02:29:54 -0600

    stripe

--- api/v1/router/general/stripe.js
+++ api/v1/router/general/stripe.js
@@ -18,6 +18,96 @@ router.post('/create-checkout-session', async (req, res) => {
   }
 });
 
+router.post('/create-user-stripe', async (req, res) => {
+  try {
+
+    const { email, id_agente } = req.body
+    const customer = await stripe.customers.create({ email });
+    const { error } = await executeQuery(
+      "INSERT INTO clientes_stripe (id_agente, id_cliente_stripe) VALUES (?, ?)",
+      [id_agente, customer.id]
+    );
+    if (error) {
+      throw new Error('Error al almacenar el Log del payment');
+    }
+    console.log("Agente creado con Customer ID:", customer.id);
+    res.json({ success: true, message: 'Customer id almacenado correctamente' });
+
+  } catch (error) {
+    console.log(error)
+    res.json(error)
+  }
+});
+
+router.get("/get-payment-methods", async (req, res) => {
+  try {
+    const { id_agente } = req.query;
+    if (!id_agente) {
+      return res.status(400).json({ error: "Falta el parámetro id_agente" });
+    }
+
+    // Consultar el `customer_id` de la base de datos
+    const [rows] = await executeQuery(
+      "SELECT id_cliente_stripe FROM clientes_stripe WHERE id_agente = ?;",
+      [id_agente]
+    ).catch(err => {
+      console.error("Database query error:", err);
+      throw new Error("Database connection error");
+    });
+
+    if (rows.length === 0) {
+      return res.status(404).json({ error: "Cliente de Stripe no encontrado para este agente" });
+    }
+    console.log(rows);
+    const customerId = rows.id_cliente_stripe;
+
+    //Consultar métodos de pago en Stripe
+    const paymentMethods = await stripe.paymentMethods.list({
+      customer: customerId,
+      type: "card", // Puedes cambiarlo según el tipo de método que necesites
+    });
+
+    res.json(paymentMethods.data);
+  } catch (error) {
+    console.error("Error obteniendo métodos de pago:", error);
+    res.status(500).json({ error: "Error en el servidor" });
+  }
+})
+
+router.post('/create-setup-intent', async (req, res) => {
+  try {
+    const { id_agente } = req.body;
+    if (!id_agente) {
+      return res.status(400).json({ error: "Falta el parámetro id_agente" });
+    }
+
+    // Consultar el `customer_id` de la base de datos
+    const [rows] = await executeQuery(
+      "SELECT id_cliente_stripe FROM clientes_stripe WHERE id_agente = ?;",
+      [id_agente]
+    ).catch(err => {
+      console.error("Database query error:", err);
+      throw new Error("Database connection error");
+    });
+
+    if (rows.length === 0) {
+      return res.status(404).json({ error: "Cliente de Stripe no encontrado para este agente" });
+    }
+    console.log(rows);
+    const customerId = rows.id_cliente_stripe;
+
+    const setupIntent = await stripe.setupIntents.create({
+      customer: customerId,
+      payment_method_types: ['card'],
+    });
+    console.log(setupIntent);
+    res.json({ client_secret: setupIntent.client_secret });
+  } catch (error) {
+    console.error('Error creating setup intent:', error);
+    res.status(500).json({ error: 'Error al crear Setup Intent' });
+  }
+});
+
 router.get("/get-checkout-session", async (req, res) => {
   try {
 
