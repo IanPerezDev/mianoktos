c909af7fd02f067dcc61d4f9b381bb02902d65ea
juanincho <sabbaghjuan7@gmail.com>
2025-04-02 14:15:50 -0600

    cambio de direccion para empresas, ruta para quitar metodos de pago del stripe

--- api/v1/router/mia/empresas.js
+++ api/v1/router/mia/empresas.js
@@ -2,7 +2,7 @@ const router = require("express").Router()
 const controller = require("../../controller/empresas")
 const middleware = require("../../middleware/validateParams")
 
-router.post("/", middleware.validateParams(["agente_id", "razon_social", "nombre_comercial", "direccion", "tipo_persona"]), controller.create)
+router.post("/", middleware.validateParams(["agente_id", "razon_social", "nombre_comercial", "tipo_persona"]), controller.create)
 
 router.get("/", controller.read)
 
