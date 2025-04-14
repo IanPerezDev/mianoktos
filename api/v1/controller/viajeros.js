61d2e05bbe561141cf35fb457f1aef9c8056590a
IanPerezDev <ian.perez@noktos.com>
2025-04-14 15:11:29 -0600

    Subiendo endpoint get viajeros by id agente

--- api/v1/controller/viajeros.js
+++ api/v1/controller/viajeros.js
@@ -1,3 +1,4 @@
+const { executeSP } = require("../../../config/db");
 const model = require("../model/viajeros");
 
 const create = async (req, res) => {
@@ -19,8 +20,21 @@ const read = async (req, res) => {
     res.status(500).json({ error: 'Error en el servidor', details: error })
   }
 }
+const get_viajeros_by_id_agente = async (req,res) => {
+  const {id_agente} = req.params;
 
+  try {
+    const viajeros= await executeSP("get_viajeros_by_id_agente",[id_agente]);
+    if (!viajeros) {
+     res.estatus(404).json({message: "No se han recuperado viajeros a partir de ese agente, intente con otro"}); 
+    } else {
+      res.status(200).json({message: "Viajeros recuperados con exito ",data: viajeros });
+    }
+  } catch (error) {
+    res-(500).json({message: "Error interno del servidor", error: error});
+  }
+}
 module.exports = {
   create,
-  read
+  read, get_viajeros_by_id_agente
 }
--- api/v1/router/mia/viajeros.js
+++ api/v1/router/mia/viajeros.js
@@ -4,5 +4,6 @@ const controller = require("../../controller/viajeros")
 
 router.post("/", middleware.validateParams(["id_empresas", "primer_nombre", "apellido_paterno", "correo"]), controller.create)
 router.get("/", controller.read)
+router.get("/get-viajeros-by-agente/:id_agente",controller.get_viajeros_by_id_agente)
 
 module.exports = router
