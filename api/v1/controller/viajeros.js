11086f71c3bc63f21ede168fb6f9c2c5749052ed
IanPerezDev <ian.perez@noktos.com>
2025-04-14 17:40:18 -0600

    Subiendo segundo enspoint, primer empresa y viajeros creados por un agente

--- api/v1/controller/viajeros.js
+++ api/v1/controller/viajeros.js
@@ -31,10 +31,29 @@ const get_viajeros_by_id_agente = async (req,res) => {
       res.status(200).json({message: "Viajeros recuperados con exito ",data: viajeros });
     }
   } catch (error) {
-    res-(500).json({message: "Error interno del servidor", error: error});
+    res.status(500).json({message: "Error interno del servidor", error: error});
+  }
+}
+
+const primeros_empresa_viajero = async (req,res) => {
+  const {id_agente} = req.params;
+ console.log(id_agente)
+  try {
+    console.log("Vamos a hacer la query");
+    const primeros = await executeSP("get_primer_viajero_y_empresa_by_agente",[id_agente]);
+    console.log("Verificando que trae primeros", primeros)
+    if (!primeros) {
+      res.estatus(404).json({message: "No se han recuperado viajeros a partir de ese agente, intente con otro"});
+    } else {
+      res.status(200).json({message: "Viajero y empresa recuperados con exito ",data: primeros });
+      
+    }
+  } catch (error) {
+    res.status(500).json({message: "Error interno del servidor", error: error});
+
   }
 }
 module.exports = {
   create,
-  read, get_viajeros_by_id_agente
+  read, get_viajeros_by_id_agente,primeros_empresa_viajero
 }
