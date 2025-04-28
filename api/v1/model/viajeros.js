72fdd38cfcc182ed8fac0af42982b7d5db95e30e
Angel castañeda <luis.castaneda@noktos.com>
2025-04-28 10:00:19 -0600

    CHANGE

--- api/v1/model/viajeros.js
+++ api/v1/model/viajeros.js
@@ -1,12 +1,13 @@
-const { executeQuery, executeTransaction } = require("../../../config/db")
-const { v4: uuidv4 } = require("uuid")
+const { executeQuery, executeTransaction } = require("../../../config/db");
+const { v4: uuidv4 } = require("uuid");
 
 const createViajero = async (viajero) => {
   try {
     const id_viajero = `via-${uuidv4()}`;
 
     // Insertar el viajero en la tabla "viajeros"
-    const query = "INSERT INTO viajeros (id_viajero, primer_nombre, segundo_nombre, apellido_paterno, apellido_materno, correo, fecha_nacimiento, genero, telefono, nacionalidad, numero_pasaporte, numero_empleado) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+    const query =
+      "INSERT INTO viajeros (id_viajero, primer_nombre, segundo_nombre, apellido_paterno, apellido_materno, correo, fecha_nacimiento, genero, telefono, nacionalidad, numero_pasaporte, numero_empleado) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
     const params = [
       id_viajero,
       viajero.primer_nombre,
@@ -26,7 +27,8 @@ const createViajero = async (viajero) => {
       console.log("Viajero creado correctamente");
 
       if (viajero.id_empresas && viajero.id_empresas.length > 0) {
-        const query2 = "INSERT INTO viajero_empresa (id_viajero, id_empresa) VALUES (?, ?);";
+        const query2 =
+          "INSERT INTO viajero_empresa (id_viajero, id_empresa) VALUES (?, ?);";
 
         for (const id_empresa of viajero.id_empresas) {
           const params2 = [id_viajero, id_empresa];
@@ -95,10 +97,14 @@ const updateViajero = async (viajero) => {
         console.log("Relaciones anteriores eliminadas");
 
         // Luego insertamos las nuevas relaciones
-        const insertQuery = "INSERT INTO viajero_empresa (id_viajero, id_empresa) VALUES (?, ?);";
-        
+        const insertQuery =
+          "INSERT INTO viajero_empresa (id_viajero, id_empresa) VALUES (?, ?);";
+
         for (const id_empresa of viajero.id_empresas) {
-          await connection.execute(insertQuery, [viajero.id_viajero, id_empresa]);
+          await connection.execute(insertQuery, [
+            viajero.id_viajero,
+            id_empresa,
+          ]);
         }
         console.log("Nuevas relaciones viajero-empresa creadas");
       }
@@ -113,32 +119,28 @@ const updateViajero = async (viajero) => {
   }
 };
 
-
 const readViajero = async () => {
   try {
-    const query = "SELECT * FROM viajeros"
-    const response = executeQuery(query)
-    return response
+    const query = "SELECT * FROM viajeros";
+    const response = executeQuery(query);
+    return response;
   } catch (error) {
     throw error;
   }
-}
+};
 const readViajeroById = async (id) => {
   try {
-    const query = `select viajeros.* from viajero_empresa 
-join viajeros on viajero_empresa.id_viajero = viajeros.id_viajero
-join empresas on viajero_empresa.id_empresa = empresas.id_empresa
-where empresas.id_empresa = ?; `
-    const response = executeQuery(query, [id])
-    return response
+    const query = `select * from viajeros_con_empresas_con_agentes where id_agente = ?;`;
+    const response = executeQuery(query, [id]);
+    return response;
   } catch (error) {
     throw error;
   }
-}
+};
 
 module.exports = {
   readViajero,
   createViajero,
   readViajeroById,
   updateViajero,
-}
+};
