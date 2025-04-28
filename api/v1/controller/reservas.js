53e06ccecef4239d89b8f6728bcf6233956c83ae
Angel castañeda <luis.castaneda@noktos.com>
2025-04-28 09:39:17 -0600

    DONE reservas

--- api/v1/controller/reservas.js
+++ api/v1/controller/reservas.js
@@ -1,47 +1,71 @@
-const model = require("../model/reservas")
+const model = require("../model/reservas");
 
 const create = async (req, res) => {
   try {
-    let response = await model.insertarReserva(req.body)
-    res.status(201).json({ message: "Solicitud created successfully", data: response })
+    let response = await model.insertarReserva(req.body);
+    res
+      .status(201)
+      .json({ message: "Solicitud created successfully", data: response });
   } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: 'Internal Server Error', details: error.message })
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", details: error.message });
   }
-}
+};
 
 const createFromOperaciones = async (req, res) => {
   try {
-    let response = await model.createReservaFromOperaciones(req.body)
-    res.status(201).json({ message: "Solicitud created successfully", data: response })
+    let response = await model.createReservaFromOperaciones(req.body);
+    res
+      .status(201)
+      .json({ message: "Solicitud created successfully", data: response });
   } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: 'Internal Server Error', details: error.message })
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", details: error.message });
   }
-}
+};
 
 const read = async (req, res) => {
   try {
-    let response = await model.getReserva()
-    res.status(201).json(response)
+    let response = await model.getReserva();
+    res.status(201).json(response);
   } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: 'Internal Server Error', details: error.message })
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", details: error.message });
   }
-}
+};
 const readById = async (req, res) => {
   try {
-    let response = await model.getReservaById(req.query.id)
-    res.status(201).json(response)
+    let response = await model.getReservaById(req.query.id);
+    res.status(201).json(response);
   } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: 'Internal Server Error', details: error.message })
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", details: error.message });
   }
-}
+};
+const readAll = async (req, res) => {
+  try {
+    let response = await model.getReservaAll(req.query.id);
+    res.status(201).json(response);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", details: error.message });
+  }
+};
 
 module.exports = {
   create,
   read,
   readById,
-  createFromOperaciones
-}
+  readAll,
+  createFromOperaciones,
+};
--- api/v1/router/mia/reservas.js
+++ api/v1/router/mia/reservas.js
@@ -1,12 +1,37 @@
-const router = require("express").Router()
-const controller = require("../../controller/reservas")
-const middleware = require("../../middleware/validateParams")
+const router = require("express").Router();
+const controller = require("../../controller/reservas");
+const middleware = require("../../middleware/validateParams");
 
-const requiredParamsToCreate = ["estado", "check_in", "check_out", "id_viajero", "nombre_hotel", "total", "subtotal", "impuestos", "tipo_cuarto", "noches", "costo_subtotal", "costo_total", "costo_impuestos", "codigo_reservacion_hotel", "items"]
+const requiredParamsToCreate = [
+  "estado",
+  "check_in",
+  "check_out",
+  "id_viajero",
+  "nombre_hotel",
+  "total",
+  "subtotal",
+  "impuestos",
+  "tipo_cuarto",
+  "noches",
+  "costo_subtotal",
+  "costo_total",
+  "costo_impuestos",
+  "codigo_reservacion_hotel",
+  "items",
+];
 
-router.post("/operaciones", middleware.validateParams(requiredParamsToCreate), controller.createFromOperaciones)
-router.post("/", middleware.validateParams(requiredParamsToCreate), controller.create)
-router.get("/", controller.read)
-router.get("/agente", controller.readById)
+router.post(
+  "/operaciones",
+  middleware.validateParams(requiredParamsToCreate),
+  controller.createFromOperaciones
+);
+router.post(
+  "/",
+  middleware.validateParams(requiredParamsToCreate),
+  controller.create
+);
+router.get("/", controller.read);
+router.get("/agente", controller.readById);
+router.get("/all", controller.readAll);
 
-module.exports = router
+module.exports = router;
