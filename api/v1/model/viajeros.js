2732cd40e74369f2f7da5efece17037f7b1fbb3b
Angel castañeda <luis.castaneda@noktos.com>
2025-05-19 11:25:03 -0600

    ADD viajero

--- api/v1/model/viajeros.js
+++ api/v1/model/viajeros.js
@@ -130,7 +130,8 @@ const readViajero = async () => {
 };
 const readViajeroById = async (id) => {
   try {
-    const query = `select * from viajeros_con_empresas_con_agentes where id_agente = ?;`;
+    const query = `select CONCAT_WS(' ', primer_nombre, segundo_nombre, apellido_paterno, apellido_materno) AS nombre_viajero_completo , id_viajero, correo, genero, fecha_nacimiento, telefono, nacionalidad, numero_pasaporte, numero_empleado
+from viajeros_con_empresas_con_agentes where id_agente = ?;`;
     const response = executeQuery(query, [id]);
     return response;
   } catch (error) {
@@ -149,7 +150,7 @@ const deleteViajero = async (id_viajero) => {
   } catch (error) {
     throw error;
   }
-}
+};
 
 module.exports = {
   readViajero,
