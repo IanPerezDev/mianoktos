d9113f3fb8ebfa8b0bbd1542bb80423c9348acd6
Angel castañeda <luis.castaneda@noktos.com>
2025-05-28 13:57:42 -0600

    ADD agentes

--- api/v1/router/mia/agentes.js
+++ api/v1/router/mia/agentes.js
@@ -22,24 +22,84 @@ router.get(
 router.get("/agentes", middleware.validateParams([]), controller.readAgentes);
 router.get("/get-agente-id/", controller.getAgenteId);
 
-//En proceso de prueba y error
-//Extrae a todos los agentes con los datos del primer viajero que tienen
+//ESTE SI LO OCUPO
 router.get("/all", async (req, res) => {
   try {
-    const query = `
+    console.log(req.query);
+    const { query } = req;
+    const { filterType = "Creacion" } = query;
+    let conditions = [];
+    let values = [];
+    let type_filters = {
+      "Check-in": "a.created_at",
+      "Check-out": "a.created_at",
+      Transaccion: "a.created_at",
+      Creacion: "a.created_at",
+    };
+
+    if (query.startDate && query.endDate) {
+      conditions.push(`${type_filters[filterType]} BETWEEN ? AND ?`);
+      values.push(query.startDate, query.endDate);
+    } else if (query.startDate) {
+      conditions.push(`${type_filters[filterType]} >= ?`);
+      values.push(query.startDate);
+    } else if (query.endDate) {
+      conditions.push(`${type_filters[filterType]} <= ?`);
+      values.push(query.endDate);
+    }
+
+    if (query.startCantidad && query.endCantidad) {
+      conditions.push(`a.monto_credito BETWEEN ? AND ?`);
+      values.push(query.startCantidad, query.endCantidad);
+    } else if (query.startCantidad) {
+      conditions.push(`a.monto_credito >= ?`);
+      values.push(query.startCantidad);
+    } else if (query.endCantidad) {
+      conditions.push(`a.monto_credito <= ?`);
+      values.push(query.endCantidad);
+    }
+
+    if (query.vendedor) {
+      conditions.push(`a.vendedor LIKE ?`);
+      values.push(`%${query.vendedor.split(" ").join("%")}%`);
+    }
+    if (query.notas) {
+      conditions.push(`a.notas LIKE ?`);
+      values.push(`%${query.notas.split(" ").join("%")}%`);
+    }
+    if (query.estado_credito) {
+      conditions.push(`a.tiene_credito_consolidado = ?`);
+      values.push(query.estado_credito == "Activo" ? 1 : 0);
+    }
+    if (query.telefono) {
+      conditions.push(`vw.telefono LIKE ?`);
+      values.push(`%${query.telefono.toString().split(" ").join("%")}%`);
+    }
+    if (query.correo) {
+      conditions.push(`vw.correo LIKE ?`);
+      values.push(`%${query.correo.toString().split(" ").join("%")}%`);
+    }
+    if (query.client) {
+      conditions.push(
+        `(CONCAT_WS(' ', vw.primer_nombre, vw.segundo_nombre, vw.apellido_paterno, vw.apellido_materno) LIKE ? OR a.id_agente LIKE ?)`
+      );
+      values.push(`%${query.client.split(" ").join("%")}%`);
+      values.push(`%${query.client.split(" ").join("%")}%`);
+    }
+
+    const queryget = `
 select
   vw.*,
 	CONCAT_WS(' ', vw.primer_nombre, vw.segundo_nombre, vw.apellido_paterno, vw.apellido_materno) AS nombre_agente_completo,
-  a.tiene_credito_consolidado,
-  a.monto_credito,
-  a.notas,
-  a.vendedor,
-  a.created_at
+  a.*
 FROM agentes as a
 JOIN vw_details_agente as vw ON vw.id_agente = a.id_agente
+WHERE vw.correo is not null ${
+      conditions.length ? "AND " + conditions.join(" AND ") : ""
+    }
 order by a.created_at desc;
 `;
-    const response = await executeQuery(query);
+    const response = await executeQuery(queryget, values);
     res.status(200).json(response);
   } catch (error) {
     console.log(error);
